7be3ede52348ff92eca443334ef4c24a01c7abfe
siameseguy <psyhakhom@gmail.com>
2018-05-16 13:49:06 -0400

    got board to show

--- game/index.js
+++ game/index.js
@@ -1,6 +1,31 @@
 import {Map} from 'immutable'
+let board = Map()
 
-export default function reducer(state, action) {
+//onst move = (turn, [row, col]) ACTION TYPE
+const MOVE = 'MOVE'
+
+// ACTION CREATOR
+export const move = function (player, position) {
+
+  return {type: MOVE, player, position}
+}
+
+export default function reducer(state = {
+  board,
+  turn: 'X'
+}, action) {
   // TODO
-  return state
+  switch (action.type) {
+    case MOVE:
+      const board = state
+        .board
+        .setIn(action.position, action.player)
+        const turn = state.turn === 'X'
+        ? 'O'
+        : 'X'
+      return {turn, board}
+    default:
+      return state
+  }
+
 }
